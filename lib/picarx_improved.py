e42b925031d1b9e28e08683dac5989455c9a5248
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2022-01-15 21:32:21 -0800

    [pp] reduced sleep duration

--- lib/picarx_improved.py
+++ lib/picarx_improved.py
@@ -250,7 +250,7 @@ class Picarx(object):
             self.set_dir_servo_angle(steer_angle)
             time.sleep(0.1)
             self.backward(35)
-            time.sleep(0.25)
+            time.sleep(0.15)
             steer_angle += 5.0
         
         self.backward(35)
@@ -259,11 +259,11 @@ class Picarx(object):
         time.sleep(0.1)
             
         steer_angle = 20.0
-        while steer_angle != -5.0:
+        while steer_angle != -10.0:
             self.set_dir_servo_angle(steer_angle)
             time.sleep(0.1)
             self.backward(35)
-            time.sleep(0.25)
+            time.sleep(0.15)
             steer_angle -= 5.0
             
         # self.forward(25)
