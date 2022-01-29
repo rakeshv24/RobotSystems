afdaadbc74d4cbfb68e639d2d56530868d66609d
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2022-01-28 23:40:33 -0800

    [lf_camera] potential fix

--- scripts/lane_following_camera.py
+++ scripts/lane_following_camera.py
@@ -6,14 +6,15 @@ import logging
 from logdecorator import log_on_start, log_on_end, log_on_error
 from picarx_improved import Picarx
 from camera import Camera
+from picamera.array import PiRGBArray
 import cv2
   
 
 if __name__=="__main__":
     px = Picarx()
     camera = Camera()
-    
     px.set_camera_servo2_angle(-15)
+    scaling_factor = 0.25
 
     while True:
         frame = camera.raw_capture()
@@ -27,8 +28,9 @@ if __name__=="__main__":
         steering_angle = camera.get_steering_angle(frame_array, lane_lines)
         heading_img = camera.display_heading_line(lines_img, steering_angle)
         cv2.imshow('heading', heading_img)
-
-        px.set_dir_servo_angle(-steering_angle)
+        
+        turn = -1 * scaling_factor * steering_angle
+        px.set_dir_servo_angle(int(turn))
 
         key = cv2.waitKey(1) & 0xFF
         if key == 27:
@@ -36,3 +38,5 @@ if __name__=="__main__":
             camera.camera.close()
             px.stop()
             break
+        
+        camera.raw_cap = PiRGBArray(camera.camera, size=camera.camera.resolution)
