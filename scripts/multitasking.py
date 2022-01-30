8c81ac6167601d9885dd9aa5478acffa603c6b97
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2022-01-29 22:24:21 -0800

    move car

--- scripts/multitasking.py
+++ scripts/multitasking.py
@@ -26,19 +26,16 @@ if __name__ == "__main__":
     delay = 0.5
     timeout = 3.0 + time.time()
 
-    while True:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-            sensor_ex = executor.submit(sensor.sensor_thread, sense_bus, delay)
-            interpret_ex = executor.submit(interpreter.interpreter_thread, sense_bus, interpret_bus, delay)
-            control_ex = executor.submit(controller.controller_thread, interpret_bus, control_bus, delay)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        sensor_ex = executor.submit(sensor.sensor_thread, sense_bus, delay)
+        interpret_ex = executor.submit(interpreter.interpreter_thread, sense_bus, interpret_bus, delay)
+        control_ex = executor.submit(controller.controller_thread, interpret_bus, control_bus, delay)
 
-        sensor_ex.result()
-        interpret_ex.result()
-        control_ex.result()
-        
+    sensor_ex.result()
+    interpret_ex.result()
+    control_ex.result()
+    
+    while time.time() < timeout:    
         px.forward(30)
-
-        if time.time() > timeout:
-            break
-            
-    px.stop()
+    
+    px.stop()    
