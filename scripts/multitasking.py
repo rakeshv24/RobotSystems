f56e66b5289722dca0598b8f1e07beba84749ebc
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2022-01-29 22:15:13 -0800

    [fix] wrong func calls

--- scripts/multitasking.py
+++ scripts/multitasking.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     while True:
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
             sensor_ex = executor.submit(sensor.sensor_thread, sense_bus, delay)
-            interpret_ex = executor.submit(sensor.interpreter_thread, sense_bus, interpret_bus, delay)
-            control_ex = executor.submit(sensor.controller_thread, interpret_bus, control_bus, delay)
+            interpret_ex = executor.submit(interpreter.interpreter_thread, sense_bus, interpret_bus, delay)
+            control_ex = executor.submit(controller.controller_thread, interpret_bus, control_bus, delay)
 
         sensor_ex.result()
         interpret_ex.result()
